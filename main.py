d45c9a2fe612348c8a24d0bc43cd4de1a49b9209
E A <emmanuelalade2008@gmail.com>
2022-12-01 18:26:51 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import ursina 
+from ursina import *
+game = Ursina()
+
+
+def update():
+    print("Update!")                    # Print Update every time this loop is executed
+    if held_keys['t']:                  # If t is pressed
+        print(held_keys['t'])           # Print the value
+
+
+
+
+game.run()
