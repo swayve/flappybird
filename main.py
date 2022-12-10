28a37416c276e0f11a57c9455692593a9b2fb059
E A <emmanuelalade2008@gmail.com>
2022-12-10 15:46:09 +0100

    initial commit

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from ursina import *
 game = Ursina()
 
 cube = Entity(model="quad", color=color.green, scale=(1,4), position= (5,0))
-sans_texture = load_texture('minecraft_blok.png.png')
-sans = Entity(model="quad", texture=sans_texture, scale=(1,1))
+
+sans = Entity(model="quad", texture='minecraft_blok.png.png', scale=(1,1))
 
 def update():
     if held_keys["w"]:
-        cube.y += 1 * time.dt 
+        cube.y += 3.5 * time.dt 
     if held_keys["s"]:
-        cube.y -= 1 * time.dt 
+        cube.y -= 3.5 * time.dt 
     if held_keys["a"]:
-        cube.x -= 1 * time.dt 
+        cube.x -= 3.5 * time.dt 
     if held_keys["d"]:
-        cube.x += 1 * time.dt 
+        cube.x += 3.5 * time.dt 
 
 
 game.run()
