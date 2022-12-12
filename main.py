7940dbc831ce867fefdcd9eaf45236692168fbc8
E A <emmanuelalade2008@gmail.com>
2022-12-12 21:50:23 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,57 +1,18 @@
 from ursina import *
-game = Ursina()
-
-class Test_Cube(Entity):
-    def __init__(self, Button):
-        super().__init__(
-            parent = scene, 
-            #Well here there are some problems so yeah look at the super() func again and try to add the entity and Button in one and try to see wether its possible
-            model = "cube",
-            color = color.white,
-            texture = "brick",
-            scale = (1, 1),
-            pressed_color = color.lime,
-            rotation = Vec3(0, 0, 0))
-
-                
-            
-        
+app = Ursina()
 
 
+bg = Entity(model="quad",scale=(20, 10), texture="46888871-624a3900-ce7f-11e8-808e-99fd90c8a3f4.png" )
 
-class Test_Button(Button):
+class PipeUp(Entity):
     def __init__(self):
-        super().__init__(
-            parent = scene,
-            model = "cube",
-            texture = "brick",
-            color = color.blue,
-            scale = (1, 1),
-            position = (5, 0),
-            highlight_color = color.red,
-            pressed_color = color.lime)
-    def input(self, key):
-        if self.hovered:
-            if key == "left mouse down":
-                print("BUtton pressed")
-            
-    
-            
-    
-    
+        super().__init__()
+        self.model = "quad"
+        Vec2(1, 1)
+        self.scale = (1,4)
+        self.texture = "pipe_up.png"
+        self.postion = (-.5, -.5)
+        self.collider = "quad"
 
-def update():
-    if held_keys["w"]:
-        cube.y += 3.5 * time.dt 
-    if held_keys["s"]:
-        cube.y -= 3.5 * time.dt 
-    if held_keys["a"]:
-        cube.x -= 3.5 * time.dt 
-    if held_keys["d"]:
-        cube.x += 3.5 * time.dt 
-        
-#cube = Entity(model="quad", color=color.green, scale=(1,4), position= (5,0))
-#minecraft_cube = Entity(model="cube", texture='minecraft_blok.png.png', scale=(1,1), position = (1,1), rotation = Vec3(30, 30, 30))
-test_cube = Test_Button()
-test_cube2 = Test_Cube()
-game.run()
+PipeUp()
+app.run()
