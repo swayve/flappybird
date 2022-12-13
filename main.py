49ced5b626be531d35fb332e0e26748b8c5d1a44
E A <emmanuelalade2008@gmail.com>
2022-12-13 12:50:09 +0100

    initial commit

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ from ursina import *
 app = Ursina()
 
 
-bg = Entity(model="quad",scale=(20, 10), texture="46888871-624a3900-ce7f-11e8-808e-99fd90c8a3f4.png" )
+bg = Entity(model="quad",scale=(20, 10), texture="bg.png")
 
 class PipeUp(Entity):
     def __init__(self):
         super().__init__()
         self.model = "quad"
-        Vec2(1, 1)
-        self.scale = (1,4)
-        self.texture = "pipe_up.png"
-        self.postion = (-.5, -.5)
-        self.collider = "quad"
+        self.texture = "PipeUp.png"
+        self.scale = (1,2)
+        self.position = (4,1)
 
 PipeUp()
 app.run()
